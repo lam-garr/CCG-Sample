a610a3248167a8e0b8eeed02fbf83240903d240c
Garrett <lamgarr@outlook.com>
2023-03-23 19:53:45 -0700

    Add handling for protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,18 @@ import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import HomePage from "./pages/Home";
 import GameBoard from "./pages/GameBoard";
+import Protected from "./pages/Protected";
 import "./styles/App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<Protected/>}>
+          <Route path="/play" element={<GameBoard/>}></Route>
+        </Route>
         <Route path="/log-in" element={<LoginPage/>}></Route>
         <Route path="/sign-up" element={<SignupPage/>}></Route>
-        <Route path="/play" element={<GameBoard/>}></Route>
         <Route path="/" element={<HomePage/>}></Route>
       </Routes>
     </BrowserRouter>
