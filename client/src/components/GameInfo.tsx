0911ad2506596a4bfc16118a1904b270d5083286
Garrett <lamgarr@outlook.com>
2023-03-31 21:11:53 -0700

    Add handling for opening/closing game info

--- client/src/components/GameInfo.tsx
+++ client/src/components/GameInfo.tsx
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faScroll } from "@fortawesome/free-solid-svg-icons";
 import gameInfoPropInterface from "../utils/interfaces/gameInfoPropInterface";
 import "../styles/GameInfo.css";
 
 function GameInfo(prop: gameInfoPropInterface){
+
+    const [ gameInfoOpen, setGameInfoOpen ] = useState(false);
+
+    const gameInfoOnClick = () => {
+        setGameInfoOpen(!gameInfoOpen);
+    }
+
     return(
         <section className="board-content">
-                <div className="extra-space-top"></div>
-                <div className="opp-info">
-                    <span>{`Opponent's deck size: ?`}</span>
-                    <span>{`Opponent's hand size: ?`}</span>
-                    <div className="extra-space-top"></div>
-                </div>
-                <div className="player-info">
-                    <span>{`Your deck size: ${prop.userDeck}`}</span>
-                    <span>{`Your hand hand size: ${prop.userHand}`}</span>
-                    <div className="extra-space-top"></div>
-                </div>
+                {gameInfoOpen ?
+                    (<div className="board-game-info">
+                        <div className="extra-space-top"></div>
+                        <div className="opp-info">
+                            <span>{`Opponent's deck size: ?`}</span>
+                            <span>{`Opponent's hand size: ?`}</span>
+                            <div className="extra-space-top"></div>
+                        </div>
+                        <div className="player-info">
+                            <span>{`Your deck size: ${prop.userDeck}`}</span>
+                            <span>{`Your hand hand size: ${prop.userHand}`}</span>
+                            <div className="extra-space-top"></div>
+                        </div>
+                        <button className="game-info-close-btn" onClick={gameInfoOnClick}>x</button>
+                        <div className="extra-space-bottom"></div>
+                    </div>) : 
+                    <div className="game-info-close-top">
+                        <button className="menu-btn" onClick={gameInfoOnClick}><FontAwesomeIcon className="menu-icon" icon={faScroll} size="lg"/></button>
+                    </div>
+                }
                 <span>{`mana: ${prop.mana}`}</span>
-                <div className="extra-space-bottom"></div>
                 <div>
                     <button className="snap-btn">!</button>
                 </div>
                 <div className="user-btns">
                     <div></div>
                     <button className="end-turn-btn" onClick={prop.endTurnFn}>{`end turn ${prop.turn}`}</button>
-                    <div className="retreat-btn">
-                        <button>retreat</button>
-                    </div>
+                    {gameInfoOpen ? 
+                        (<div className="retreat-btn">
+                            <button className="bottom-right-buddy">retreat</button>
+                        </div>) : 
+                        (<div className="no-retreat-btn"></div>)
+                    }
                 </div>
             </section> 
     )
